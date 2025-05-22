a8d8900ffdbaf6bb9f17ea66d47aa84ea58f647e
vermakusum593 <vermakusum593@gmail.com>
2025-05-22 23:50:22 +0200

    added all the files yml and js

---
+++ your-project/backend/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const User = require("./models/User");
+
+const app = express();
+app.use(express.json());
+
+// Connect MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.log(err));
+
+// Routes
+app.get("/api/users", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+
+app.post("/api/users", async (req, res) => {
+  const user = await User.create(req.body);
+  res.json(user);
+});
+
+app.put("/api/users/:id", async (req, res) => {
+  const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(user);
+});
+
+app.delete("/api/users/:id", async (req, res) => {
+  await User.findByIdAndDelete(req.params.id);
+  res.json({ message: "User deleted" });
+});
+
+app.listen(5000, () => console.log("Server started on port 5000"));
